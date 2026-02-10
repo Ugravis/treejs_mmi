2a1c82c90d3d7e6d32b007a48cb48cc37dc7bc7f
Ulysse Pennetier <upennetier@gmail.com>
2026-02-10 08:53:35 +0100

    feat(cours_2): déplacement avec la souris

--- cours_2/index.js
+++ cours_2/index.js
@@ -1,111 +1,113 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const mestouches = { ArrowUp: false, ArrowDown: false, ArrowLeft: false, ArrowRight: false };
 
-
 // Render & scene
-const renderer = new THREE.WebGLRenderer({canvas});
+const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.shadowMap.enabled = true;
-const scene = new THREE.Scene(); 
-
+const scene = new THREE.Scene();
 
 // Caméra
-const camera = new THREE.PerspectiveCamera(80,ratio);
-camera.position.set(0,1,5);
-
+const camera = new THREE.PerspectiveCamera(80, ratio);
+camera.position.set(0, 1, 5);
 
 // Lumière
-const pointLight = new THREE.PointLight(0xffffff, 30)
-pointLight.position.set(0, 4, 3)
-pointLight.castShadow = true
-scene.add(pointLight)
-
+const pointLight = new THREE.PointLight(0xffffff, 30);
+pointLight.position.set(0, 4, 3);
+pointLight.castShadow = true;
+scene.add(pointLight);
 
 // Ajout du plan
-const planeGeometry = new THREE.PlaneGeometry(10,10);
-const planeMaterial = new THREE.MeshStandardMaterial({color: 0x884444, side: THREE.DoubleSide});
+const planeGeometry = new THREE.PlaneGeometry(10, 10);
+const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x884444, side: THREE.DoubleSide });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = Math.PI / 2;
 plane.position.y = -1;
 plane.receiveShadow = true;
 scene.add(plane);
 
-
 // Ajout du cube
-const geometry = new THREE.BoxGeometry(1,1,1);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({ color: 0x33ff33 });
 const moncube = new THREE.Mesh(geometry, material);
 
 // Et sa texture
-const textureLoader = new THREE.TextureLoader()
-textureLoader.load('./textures/texture_foshu.jpg', (texture) => {
-    const materialTexture = new THREE.MeshStandardMaterial({ map: texture })
-    moncube.material = materialTexture
-})
+const textureLoader = new THREE.TextureLoader();
+textureLoader.load("./textures/texture_foshu.jpg", (texture) => {
+  const materialTexture = new THREE.MeshStandardMaterial({ map: texture });
+  moncube.material = materialTexture;
+});
 
-moncube.position.y = 1,5;
+((moncube.position.y = 1), 5);
 moncube.castShadow = true;
 scene.add(moncube);
 
-
 // Ajout objet 3D GLTF
-let glasses
-const loader = new GLTFLoader()
-loader.load('./assets/sunglasses.glb', function(gltf){ 
+let glasses;
+const loader = new GLTFLoader();
+loader.load("./assets/sunglasses.glb", function (gltf) {
+  gltf.scene.traverse((node) => {
+    if (node.isMesh) {
+      node.castShadow = true;
+    }
+  });
 
-    gltf.scene.traverse((node) => {
-        if (node.isMesh) {
-            node.castShadow = true;
-        }
-    });
+  glasses = gltf.scene;
+  glasses.scale.set(12, 12, 12);
+  glasses.position.x = 1.5;
+  glasses.position.z = 2;
+  glasses.position.y = 0;
 
-    glasses = gltf.scene
-    glasses.scale.set(12, 12, 12)
-    glasses.position.x = 1.5
-    glasses.position.z = 2
-    glasses.position.y = 0
-
-    scene.add(glasses);
+  scene.add(glasses);
 });
 
-
 // Updater en boucle pour l'animation
-function monUpdater(){
-    moncube.rotation.x += 0.01;
-    moncube.rotation.y += 0.015;
-    renderer.render(scene,camera);
-    requestAnimationFrame(monUpdater);
+function monUpdater() {
+  moncube.rotation.x += 0.01;
+  moncube.rotation.y += 0.015;
+  renderer.render(scene, camera);
+  requestAnimationFrame(monUpdater);
 
-    if (mestouches.ArrowUp) {
-        moncube.position.z -= 0.1;
-    }
-    if (mestouches.ArrowDown) {
-        moncube.position.z += 0.1;
-    }
-    if (mestouches.ArrowLeft) {
-        moncube.position.x -= 0.1;
-    }
-    if (mestouches.ArrowRight) {
-        moncube.position.x += 0.1;
-    }
+  // if (mestouches.ArrowUp) {
+  //   moncube.position.z -= 0.1;
+  // }
+  // if (mestouches.ArrowDown) {
+  //   moncube.position.z += 0.1;
+  // }
+  // if (mestouches.ArrowLeft) {
+  //   moncube.position.x -= 0.1;
+  // }
+  // if (mestouches.ArrowRight) {
+  //   moncube.position.x += 0.1;
+  // }
 }
 
 monUpdater();
 
-
 //
 
+const mouse = new THREE.Vector2();
+
+window.addEventListener('mousemove', function(ev) {
+  // La formule : (Position / Taille Totale) * 2 - 1
+  mouse.x = (ev.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(ev.clientY / window.innerHeight) * 2 + 1;
+
+  // On multiplie par un facteur (ici 5) pour que le mouvement soit visible à l'écran
+  moncube.position.x = mouse.x * 5;
+  moncube.position.y = mouse.y * 5;
+});
 
 // Écouteurs pour détecter les touches pressées et relâchées
-window.addEventListener('keydown', (event) => {
-  if (mestouches.hasOwnProperty(event.key)) {
-    mestouches[event.key] = true;
-  }
-});
+// window.addEventListener("keydown", (event) => {
+//   if (mestouches.hasOwnProperty(event.key)) {
+//     mestouches[event.key] = true;
+//   }
+// });
 
-window.addEventListener('keyup', (event) => {
-  if (mestouches.hasOwnProperty(event.key)) {
-    mestouches[event.key] = false;
-  }
-});
+// window.addEventListener("keyup", (event) => {
+//   if (mestouches.hasOwnProperty(event.key)) {
+//     mestouches[event.key] = false;
+//   }
+// });
